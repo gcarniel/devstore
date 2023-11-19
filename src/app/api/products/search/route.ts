e0245252f8f2bda57e52ab765a9c96ba299c9eef
Gabriel Carniel <gpcarniel@outlook.com>
2023-11-19 14:52:58 -0300

    rota de pesquisa criada

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+
+import data from '../data.json'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const { searchParams } = request.nextUrl
+
+  console.log(searchParams)
+
+  const query = z.string().parse(searchParams.get('q'))
+
+  const products = data.products.filter((product) =>
+    product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()),
+  )
+
+  if (!products) {
+    return Response.json({ message: 'Product not found.' }, { status: 400 })
+  }
+
+  return Response.json(products)
+}
